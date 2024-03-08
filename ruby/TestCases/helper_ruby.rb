036048f0e82fe84b959156a5aa90f75e48c05a03
Abhinav Saini <abhinavs@mapup.ai>
2024-03-08 14:59:00 +0530

    refactor: ruby

--- ruby/TestCases/helper_ruby.rb
+++ ruby/TestCases/helper_ruby.rb
@@ -11,12 +11,20 @@ TOLLGURU_API_KEY = ENV["TOLLGURU_API_KEY"]
 TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2"
 POLYLINE_ENDPOINT = "complete-polyline-from-mapping-service"
 
-def get_toll_rate(source,destination)
+# Explore https://tollguru.com/toll-api-docs to get the best of all the parameters that tollguru has to offer
+request_parameters = {
+  "vehicle": {
+    "type": "2AxlesAuto",
+  },
+  # Visit https://en.wikipedia.org/wiki/Unix_time to know the time format
+  "departure_time": "2021-01-05T09:46:08Z",
+}
 
+def get_toll_rate(source, destination)
     def get_coord_hash(loc)
         geocoding_url = "#{TOMTOM_GEOCODE_API_URL}/#{CGI::escape(loc)}.JSON?key=#{TOMTOM_API_KEY}&limit=1"
         coord = JSON.parse(HTTParty.get(geocoding_url).body)
-        return (coord['results'].pop)['position']
+        return coord['results'].pop['position']
     end
 
     # Source Details in latitude-longitude pair using geocoding API
@@ -26,28 +34,28 @@ def get_toll_rate(source,destination)
     destination = get_coord_hash(destination)
 
     # GET Request to TomTom for Route Coordinates
-
-    tomtom_url = "#{TOMTOM_API_URL}/#{SOURCE["lat"]},#{SOURCE["lon"]}:#{DESTINATION["lat"]},#{DESTINATION["lon"]}/json?avoid=unpavedRoads&key=#{TOMTOM_API_KEY}"
+    tomtom_url = "#{TOMTOM_API_URL}/#{source['lat']},#{source['lon']}:#{destination['lat']},#{destination['lon']}/json?avoid=unpavedRoads&key=#{TOMTOM_API_KEY}"
     response = HTTParty.get(tomtom_url).body
     json_parsed = JSON.parse(response)
 
     # Extracting lat-long pairs from JSON and converting to polyline
-    tomtom_coordinates = json_parsed['routes'].map { |x| x['legs'] }.pop.map{ |y| y['points']}.pop.map {|z| z.values}
+    tomtom_coordinates = json_parsed['routes'].map { |x| x['legs'] }.pop.map { |y| y['points'] }.pop.map { |z| z.values }
     google_encoded_polyline = FastPolylines.encode(tomtom_coordinates)
 
     # Sending POST request to TollGuru
-    tollguru_url = "#{TOLLGURU_API_URL}/#{POLYLINE_ENDPOINT}" 
-    headers = {'content-type' => 'application/json', 'x-api-key' => TOLLGURU_API_KEY}
-    body = {'source' => "tomtom", 'polyline' => google_encoded_polyline, 'vehicleType' => "2AxlesAuto", 'departure_time' => "2021-01-05T09:46:08Z"}
-    tollguru_response = HTTParty.post(tollguru_url,:body => body.to_json, :headers => headers)
+    tollguru_url = "#{TOLLGURU_API_URL}/#{POLYLINE_ENDPOINT}"
+    headers = { 'content-type' => 'application/json', 'x-api-key' => TOLLGURU_API_KEY }
+    body = { 'source' => "tomtom", 'polyline' => google_encoded_polyline, **request_parameters }
+    tollguru_response = HTTParty.post(tollguru_url, body: body.to_json, headers: headers)
+
     begin
-        toll_body = JSON.parse(tollguru_response.body)    
+        toll_body = JSON.parse(tollguru_response.body)
         if toll_body["route"]["hasTolls"] == true
-            return google_encoded_polyline,toll_body["route"]["costs"]["tag"], toll_body["route"]["costs"]["cash"] 
+            return google_encoded_polyline, toll_body["route"]["costs"]["tag"], toll_body["route"]["costs"]["cash"]
         else
             raise "No tolls encountered in this route"
         end
-    rescue Exception => e
-        puts e.message 
+    rescue StandardError => e
+        puts e.message
     end
 end
--- ruby/main.rb
+++ ruby/main.rb
@@ -3,13 +3,25 @@ require 'json'
 require 'fast_polylines'
 require 'cgi'
 
-TOMTOM_API_KEY = ENV["TOMTOM_API_KEY"]
+TOMTOM_API_KEY = "PQurHG5ismFRMfUpkyS9BmKyvQXeCv6h"
 TOMTOM_API_URL = "https://api.tomtom.com/routing/1/calculateRoute"
 TOMTOM_GEOCODE_API_URL = "https://api.tomtom.com/search/2/geocode"
 
-TOLLGURU_API_KEY = ENV["TOLLGURU_API_KEY"]
+TOLLGURU_API_KEY = "Dd6AAjtNnBrb9hrBLB9jqpF79JL8DfnA"
 TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2"
 POLYLINE_ENDPOINT = "complete-polyline-from-mapping-service"
+
+source = get_coord_array("Philadelphia, PA")
+destination = get_coord_array("New York, NY")
+
+# Explore https://tollguru.com/toll-api-docs to get the best of all the parameters that tollguru has to offer
+request_parameters = {
+  "vehicle": {
+    "type": "2AxlesAuto",
+  },
+  # Visit https://en.wikipedia.org/wiki/Unix_time to know the time format
+  "departure_time": "2021-01-05T09:46:08Z",
+}
 
 def get_coord_hash(loc)
     geocoding_url = "#{TOMTOM_GEOCODE_API_URL}/#{CGI::escape(loc)}.JSON?key=#{TOMTOM_API_KEY}&limit=1"
@@ -18,10 +30,10 @@ def get_coord_hash(loc)
 end
 
 # Source Details in latitude-longitude pair using geocoding API
-SOURCE = get_coord_hash("Dallas, TX")
+SOURCE = get_coord_hash(source)
 
 # Destination Details in latitude-longitude pair using geocoding API
-DESTINATION = get_coord_hash("New York, NY")
+DESTINATION = get_coord_hash(destination)
 
 # GET Request to TomTom for Route Coordinates
 TOMTOM_URL = "#{TOMTOM_API_URL}/#{SOURCE["lat"]},#{SOURCE["lon"]}:#{DESTINATION["lat"]},#{DESTINATION["lon"]}/json?avoid=unpavedRoads&key=#{TOMTOM_API_KEY}"
@@ -35,5 +47,10 @@ google_encoded_polyline = FastPolylines.encode(tomtom_coordinates)
 # Sending POST request to TollGuru
 tollguru_url = "#{TOLLGURU_API_URL}/#{POLYLINE_ENDPOINT}" 
 headers = {'content-type' => 'application/json', 'x-api-key' => TOLLGURU_API_KEY}
-body = {'source' => "tomtom", 'polyline' => google_encoded_polyline, 'vehicleType' => "2AxlesAuto", 'departure_time' => "2021-01-05T09:46:08Z"}
+body = {
+  'source': "tomtom",
+  'polyline': google_encoded_polyline,
+  **request_parameters,
+}
 tollguru_response = HTTParty.post(tollguru_url,:body => body.to_json, :headers => headers)
+puts tollguru_response
