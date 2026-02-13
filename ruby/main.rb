05410ffb70914b76ac3bef5014db475222bd9145
Asijit Paul <asijitp@mapup.ai>
2026-02-13 17:51:32 +0530

    refactor: Update HTTP client implementations and API key handling to use environment variables across Ruby, JavaScript, and Python examples, and expand gitignore.

--- ruby/main.rb
+++ ruby/main.rb
@@ -1,56 +1,86 @@
-require 'HTTParty'
+require 'uri'
+require 'net/http'
 require 'json'
 require 'fast_polylines'
 require 'cgi'
 
-TOMTOM_API_KEY = "PQurHG5ismFRMfUpkyS9BmKyvQXeCv6h"
+TOMTOM_API_KEY = ENV['TOMTOM_API_KEY']
 TOMTOM_API_URL = "https://api.tomtom.com/routing/1/calculateRoute"
 TOMTOM_GEOCODE_API_URL = "https://api.tomtom.com/search/2/geocode"
 
-TOLLGURU_API_KEY = "Dd6AAjtNnBrb9hrBLB9jqpF79JL8DfnA"
+TOLLGURU_API_KEY = ENV['TOLLGURU_API_KEY']
 TOLLGURU_API_URL = "https://apis.tollguru.com/toll/v2"
 POLYLINE_ENDPOINT = "complete-polyline-from-mapping-service"
 
-source = get_coord_array("Philadelphia, PA")
-destination = get_coord_array("New York, NY")
-
-# Explore https://tollguru.com/toll-api-docs to get the best of all the parameters that tollguru has to offer
-request_parameters = {
-  "vehicle": {
-    "type": "2AxlesAuto",
-  },
-  # Visit https://en.wikipedia.org/wiki/Unix_time to know the time format
-  "departure_time": "2021-01-05T09:46:08Z",
-}
-
 def get_coord_hash(loc)
-    geocoding_url = "#{TOMTOM_GEOCODE_API_URL}/#{CGI::escape(loc)}.JSON?key=#{TOMTOM_API_KEY}&limit=1"
-    coord = JSON.parse(HTTParty.get(geocoding_url).body)
+  uri = URI("#{TOMTOM_GEOCODE_API_URL}/#{CGI::escape(loc)}.JSON?key=#{TOMTOM_API_KEY}&limit=1")
+  response = Net::HTTP.get_response(uri)
+  if response.is_a?(Net::HTTPSuccess)
+    coord = JSON.parse(response.body)
     return (coord['results'].pop)['position']
+  else
+    puts "Error fetching coordinates: #{response.code} #{response.message}"
+    return nil
+  end
 end
 
-# Source Details in latitude-longitude pair using geocoding API
-SOURCE = get_coord_hash(source)
+source_loc = "Philadelphia, PA"
+destination_loc = "New York, NY"
 
-# Destination Details in latitude-longitude pair using geocoding API
-DESTINATION = get_coord_hash(destination)
+request_parameters = {
+  "vehicle" => {
+    "type" => "2AxlesAuto",
+  },
+  "departure_time" => "2021-01-05T09:46:08Z",
+}
 
-# GET Request to TomTom for Route Coordinates
-TOMTOM_URL = "#{TOMTOM_API_URL}/#{SOURCE["lat"]},#{SOURCE["lon"]}:#{DESTINATION["lat"]},#{DESTINATION["lon"]}/json?avoid=unpavedRoads&key=#{TOMTOM_API_KEY}"
-RESPONSE = HTTParty.get(TOMTOM_URL).body
-json_parsed = JSON.parse(RESPONSE)
+# Step 1: Get Coordinates
+source = get_coord_hash(source_loc)
+destination = get_coord_hash(destination_loc)
 
-# Extracting lat-long pairs from JSON and converting to polyline
+if source.nil? || destination.nil?
+  puts "Failed to get coordinates."
+  exit
+end
+
+# Step 2: Get Route from TomTom
+tomtom_url_string = "#{TOMTOM_API_URL}/#{source["lat"]},#{source["lon"]}:#{destination["lat"]},#{destination["lon"]}/json?avoid=unpavedRoads&key=#{TOMTOM_API_KEY}"
+tomtom_uri = URI(tomtom_url_string)
+response_tomtom = Net::HTTP.get_response(tomtom_uri)
+
+if !response_tomtom.is_a?(Net::HTTPSuccess)
+  puts "TomTom API Error: #{response_tomtom.code}"
+  puts response_tomtom.body
+  exit
+end
+
+json_parsed = JSON.parse(response_tomtom.body)
 tomtom_coordinates = json_parsed['routes'].map { |x| x['legs'] }.pop.map{ |y| y['points']}.pop.map {|z| z.values}
 google_encoded_polyline = FastPolylines.encode(tomtom_coordinates)
 
 # Sending POST request to TollGuru
-tollguru_url = "#{TOLLGURU_API_URL}/#{POLYLINE_ENDPOINT}" 
-headers = {'content-type' => 'application/json', 'x-api-key' => TOLLGURU_API_KEY}
+tollguru_url = "#{TOLLGURU_API_URL}/#{POLYLINE_ENDPOINT}"
+
 body = {
-  'source': "tomtom",
-  'polyline': google_encoded_polyline,
-  **request_parameters,
-}
-tollguru_response = HTTParty.post(tollguru_url,:body => body.to_json, :headers => headers)
-puts tollguru_response
+  'source' => "tomtom",
+  'polyline' => google_encoded_polyline
+}.merge(request_parameters)
+
+File.write('tollguru_request_body.json', body.to_json)
+
+puts "Sending request to TollGuru via curl..."
+command = "curl -s -X POST #{tollguru_url} -H 'Content-Type: application/json' -H 'x-api-key: #{TOLLGURU_API_KEY}' -d @tollguru_request_body.json"
+response_body = `#{command}`
+
+File.delete('tollguru_request_body.json')
+
+begin
+  tollguru_response = JSON.parse(response_body)
+  if tollguru_response['route'] && tollguru_response['route']['costs']
+    puts "The rates are \n #{tollguru_response['route']['costs']}"
+  else
+    puts "Response: #{response_body}"
+  end
+rescue JSON::ParserError => e
+  puts "Failed to parse response: #{response_body}"
+end
